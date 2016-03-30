97fde0a743d25fce9c7e5d45868b2176f7651182
Samuel Doud <sd8396a@student.american.edu>
2016-03-30 18:16:55 -0400

    matplotlib class added
    This class is the display of the homotopy

---
+++ plot.py
@@ -0,0 +1,28 @@
+import PointGrid
+import Line
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from sympy import re, im
+
+class plot(object):
+    def __init__(self, x_min, x_max, y_min, y_max):
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min,y_max)
+        plt.xlabel('Real')
+        plt.ylabel('Imaginary')
+        self.grid=PointGrid()
+
+    def AddLine(self, line):
+        #add a line to the pointgrid
+        self.grid.addLine(line)
+
+    def NextFrame(self):
+        complex_lines=self.grid.newLines() #get all the new lines from the grid. The PlotGrid calls to each Line object asking for new points, the Line then calls for new locations from each point based on their deformation pattern.
+        for line in complex_lines:
+            reals = [re(complex) for complex in line]
+            imaginaries = [im(complex) for complex in line] #list comps for the real and imaginary parts of the line
+            plt.plot(reals,imaginaries,line.color) #plot this single line on the graph
+
+
+        
