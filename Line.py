12a43d07a19b49a8ec345d6bf6cd1f4b08a27855
Samuel Doud <sd8396a@student.american.edu>
2016-03-29 20:43:47 -0400

    Added a line object
    Allows for a line to be known to be simply connected without just
    attaching another starting point to the end of the list of points. Also
    allows for intelligent deletion.

---
+++ Line.py
@@ -0,0 +1,8 @@
+﻿class Line(object):
+    """description of class"""
+    def __init__(self, name, points, simply_connected=False):
+        self.simply_connected=simply_connected
+        self.name=name
+        self.points=points
+        self.count=points
+
