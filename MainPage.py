256df541cecc7706933ad859b147fe16354262bb
sammy <sammy@DESKTOP-UMSRKEC>
2016-04-04 14:01:27 -0400

    Function evaluation is functional

--- MainPage.py
+++ MainPage.py
@@ -1,10 +1,34 @@
-import tkinter
+import tkinter as tk
 import PointGrid
 import plot
 import Line
+import function as func
+import cmath
+import math
+from sympy import Symbol, symbols
+z=symbols('z',complex=True)
+functionObj = func.function(z**2)
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-def init():
-    window = tkinter.Tk()
-    window.mainloop() #This launches the main window
+    def createWidgets(self):
+        self.hi_there = tk.Button(self)
+        z=complex(1,1)
+        self.hi_there["text"] = "f("+ str(z) + ")=" + str(functionObj.evaluateAt(z))
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
 
+        self.QUIT = tk.Button(self, text="QUIT", fg="red",
+                                            command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
--- function.py
+++ function.py
@@ -1,18 +1,15 @@
-﻿from sympy import I, re, imageop, Abs, arg, conjugate, expand, rewrite
+﻿from sympy import I, re, im, Abs, arg, conjugate, expand, Symbol, symbols, lambdify
+from sympy.abc import z
 import cmath
-z = symbols('z', complex=True) #establish z as a complex number
+import numpy
 class function(object):
     """description of class"""
     def __init__(self, expression):
         """Take an expression and parse it"""
-        expression = str(expression)
-        expression.replace("i", "I") #sympy prefers uppercase I to denote the imaginary unit
-        self.expression = expression.expand(complex=True)
-        self.real_part = re(expression)
-        self.imag_part = im(expression)
-    def evaluateAt(z):
-        real_z = z.real
-        imag_z = z.imag #break up z in to its real and imaginary parts
-        real_f_z = self.real_part.subs({z:real_z}).n()
-        imag_f_z = self.imag_part.subs({z:imag_z}).n()#take the real and imaginary parts of the expression and evaluate them with respect to re(z) and im(z)
-        return complex(real_f_z, imag_f_z) #return the result!
+        self.z=symbols('z',complex=True)
+        self.expression=expression
+        self.f_z=lambdify(self.z, self.expression, "numpy")
+        
+    def evaluateAt(self,w):
+        return complex(self.f_z(w)) #return the result!
+    
