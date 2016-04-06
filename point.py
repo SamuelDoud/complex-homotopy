65dcc3e3870033f73d78b5943c4e7110a0d1d9f0
Samuel Doud <sd8396a@student.american.edu>
2016-04-06 13:33:04 -0400

    Wow. re(z) is not NEARLY as fast as z.real! Assume the same for im
    >>> from sympy import re, im
    >>> import time
    >>> import numpy as np
    >>> data = [complex(x**2, x*3) for x in np.linspace(0,50,1000)]
    >>> def test_re_z(data):
    start_time = time.time()
    [re(z) for z in data]
    print("--- %s seconds ---" % (time.time() - start_time))
    
    >>> def test_z_dot_real(data):
    start_time = time.time()
    [z.real for z in data]
    print("--- %s seconds ---" % (time.time() - start_time))
    
    >>> test_z_dot_real(data)
    --- 0.0005040168762207031 seconds ---
    >>> test_re_z(data)
    --- 7.3745598793029785 seconds ---

--- point.py
+++ point.py
@@ -1,4 +1,4 @@
-﻿from sympy import re, im
+﻿import numpy as np
 REAL=0
 IMAG=1#constants used for point order indexes
 
@@ -7,27 +7,27 @@ class point(object):
     User must pass a real and imaginary starting position to the point object"""
 
     def __init__(self, z):
-        self.real = z.real
-        self.imag = z.imag
-        self.complex=z
-        self.point_order = [[],[]]
-        self.full_point_order = [[],[]]
+        self.complex=z #save the point's complex number
+        self.point_order = [[],[]] #where the point's homotopies will go
+        self.full_point_order = [[],[]] #full order is a list that is 2 point orders attached by their tails. Think of the cartoon character CatDog, but just DogDog or CatCat
 
     def parameterize(self, f_z, n_steps):
+        """given the value of this point applied to a function, parameterize its path to that point on the complex plane"""
         total_list=[[],[]]
-        for t in n_steps.linspace(0,1,n_steps+1):
-            z=(1-t)*self.complex + t*f_z
-            total_list[REAL].append(re(z))
-            total_list[IMAG].append(im(z))
-        self.point_order=total_list
-        self.full_point_order[REAL] = add_reverse(self.point_order[REAL])
+        for t in np.linspace(0,1,n_steps+1):#range t from 0 to 1 inclusive with n_step + 1 number of evenly spaced steps
+            z=(1-t)*self.complex + t*f_z #Dr. Casey's method of parameterizing the homotopy. This is kinda the meat of the entire program
+            total_list[REAL].append(z.real) #append the point calculated to the points order
+            total_list[IMAG].append(z.imag)
+        self.point_order=total_list #point order is the non-reversed list
+        self.full_point_order[REAL] = add_reverse(self.point_order[REAL]) #this allows the animation to run in reverse
         self.full_point_order[IMAG] = add_reverse(self.point_order[IMAG])
 
     def data_dump(self):
-        return {"reals":point_order[REAL],"imaginaries":point_order[IMAG]}
+        return {"reals":point_order[REAL],"imaginaries":point_order[IMAG]} #only dump the non-reversed list as storing that is pretty redunadant
 
 def add_reverse(target):
     """Take a list, reverse it, and extend the original list with that"""
+    #Is there a C/C++ type way to do this with pointers instead of actually copying and using unneeded memory?
     og_target = list(target) #copy the list to avoid the next line affecting og_target
     target.reverse()
     return og_target + target
