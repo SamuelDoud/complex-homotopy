7914c24f1b8ec1730775cb46ff16d027df971059
Samuel Doud <sd8396a@student.american.edu>
2016-04-06 17:20:09 -0400

    Can graph a string now

--- MainPage.py
+++ MainPage.py
@@ -1,19 +1,25 @@
 ﻿from tkinter import *
 import PointGrid
-import plot
+import plot_window
 import function as func
 from sympy import Symbol, symbols, re, im, arg, Abs, sympify
 
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
 
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
-        self.createWidgets()
         self.unit_square = PointGrid.PointGrid(complex(-1+1j),complex(1-1j),20,20)
+        self.createWidgets()
+
     def createWidgets(self):
         self.hi_there = Button(self)
         self.function_label = Label( root, text="Enter a f(z)")
@@ -28,15 +34,28 @@ class Application(Frame):
         self.submit.pack(side =BOTTOM) 
         self.QUIT = Button(self, text="QUIT", fg="red", command=root.destroy)
         self.QUIT.pack(side="bottom")
+        self.update_graph(plot_window.plot_window(self.unit_square).fig)
 
     def say_hi(self):
         print("hi there, everyone!")
 
     def launch(self):
-        functionObj = func.function(str(self.function_entry.get()))
+        z=symbols('z',complex=True)
+        functionObj = func.function(self.function_entry.get())
         self.unit_square.provide_function(functionObj,int(self.n_entry.get()))
-        plot_window=plot.plot(self.unit_square)
-        plot_window.show()
+        plot_obj=plot_window.plot_window(self.unit_square)
+        plot_obj.show()
+        self.update_graph(plot_obj.fig)
+
+    def update_graph(self, f):  
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
+
 
 root = Tk()
 app = Application(master=root)
--- function.py
+++ function.py
@@ -3,9 +3,16 @@ from sympy import Function, Symbol, symbols, lambdify,re, im, arg, Abs
 
 class function(object):
     def __init__(self, expression):
-            self.expr=expression
-            self.z=symbols('z',complex=True)
-            self.f_z=lambdify(self.z, self.expr, "numpy") #taking advantage of the reuse of the function object. Lamdba numby operations greatly speed up operations on large amounts of data with inital overhead
+            self.use_eval=False #work to get rid of this
+            if isinstance(expression, str):
+                self.use_eval = True
+                self.expr = expression
+            else:
+                self.expr=expression
+                self.z=symbols('z',complex=True)
+                self.f_z=lambdify(self.z, self.expr, "numpy") #taking advantage of the reuse of the function object. Lamdba numby operations greatly speed up operations on large amounts of data with inital overhead
     def evaluateAt(self,z):
+        if self.use_eval:
+            return eval(self.expr)
         return self.f_z(z)
 
