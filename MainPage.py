f31bd767225926502348b0286800491feed91e1d
Samuel Doud <sd8396a@student.american.edu>
2016-04-20 15:32:18 -0400

    Added identity and cleaded some exceptions

--- MainPage.py
+++ MainPage.py
@@ -12,14 +12,19 @@ import function as func
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
+        self.identity = "z"
+        self.identity_function = func.function(self.identity)
         self.pack()
         self.master=master
         self.grid = PointGrid.PointGrid()
+        
         self.create_widgets()
         self.animating_already = False
         self.add_lines()
+        self.launch()
 
     def create_widgets(self):
+        self.save_video = Button(self, text="Save as Video", command=self.save_video_handler)
         self.function_label = Label( root, text="Enter a f(z)")
         self.function_entry = Entry(root, bd=20)
         self.n_label = Label(root, text="Number of steps")
@@ -36,6 +41,7 @@ class Application(Frame):
         self.submit.pack(side =BOTTOM) 
         self.QUIT = Button(self, text="Quit", fg="red", command=root.destroy)
         self.QUIT.pack(side=BOTTOM)
+        self.save_video.pack(side=BOTTOM)
 
     def add_lines(self, list_of_lines=None):
         if list_of_lines:
@@ -44,22 +50,36 @@ class Application(Frame):
             self.grid.circle(1)
             self.grid.grid_lines(complex(-1,1),complex(1,-1),10,10)
 
-    def launch(self):
+    def save_video_handler(self):
+        self.plot_obj.save(self.current_function, video=True)
 
+    def launch(self):
+        """
+        Create a new animation based on the data given by the user.
+        This method will be called on the press of the submit button.
+        """
+        #take the input from the user.. if its null, set ito the identity function
+        self.current_function = self.function_entry.get() if self.function_entry.get() else self.identity
         try:
-            functionObj = func.function(self.function_entry.get())
+            functionObj = func.function(self.current_function)
         except:
-            functionObj = func.function("z")#identity function
+            functionObj = self.identity_function #fallback to the identity 
         try:
             n = int(self.n_entry.get())
         except:
             n = 1 #no animation
         #interval cannot be changed after lauch
-        self.grid.provide_function(functionObj,int(self.n_entry.get()))
+        self.grid.provide_function(functionObj,n)
         
         if not self.animating_already:
             self.plot_obj=plot_window.plot_window(self.grid)
             self.update_graph(self.plot_obj)
+        else:
+            try:
+                interval = int(self.interval_entry.get())
+            except ValueError:
+                interval=20
+            self.plot_obj.anim._interval = interval
         self.plot_obj.new_limits()
         #else:
         #    self.plot_obj.new_limits() #throw the new limits in on the graph
@@ -68,9 +88,10 @@ class Application(Frame):
         self.canvas = FigureCanvasTkAgg(f.fig,master=self)
         self.canvas.show()
         self.canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-        interval=20
-        if int(self.interval_entry.get()):
+        try:
             interval = int(self.interval_entry.get())
+        except ValueError:
+            interval=20
         self.animating_already = True
         f.animate(interval_length=interval)
 
