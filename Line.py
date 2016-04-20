9b0bff7b353dd84dc53f17112d9157775de6b767
Samuel Doud <sd8396a@student.american.edu>
2016-04-20 10:15:56 -0400

    Added map to create_points and reordered imports

--- Line.py
+++ Line.py
@@ -1,6 +1,8 @@
-﻿import numpy as np
+﻿import cmath
+
+import numpy as np
+
 import point
-import cmath
 
 class Line(object):
     """Line is a container of points and delegates operations to them. Additionally, Line containss information shared among the points on a Line such as the color of the line, if it is connected, how many points are on the line etc.. Lines are held within a PointGrid"""
@@ -30,27 +32,29 @@ class Line(object):
         """
         thetas=np.linspace(0,2*np.pi,points)
         points=[point.point(cmath.rect(radius,theta)+center) for theta in thetas]
-        return cls(0,0,0,0,color,group,simply_connected=True,points=points)    
-        
+        return cls(0,0,0,0,color,group,simply_connected=True,points=points)
+
     def create_points(self):
-        z = np.linspace(self.start,self.end,self.number_of_points) #this is the first value on the domain
-        points = []
-        for step_index in range(self.number_of_points): #there is always at least a start and an end, start is already included, so end must be forced in
-            f_z = self.function.evaluateAt(z[step_index]) #evaluate the function at this complex number
-            points.append(point.point(f_z)) #create a point located at
-        return points
+        """
+        Taking a function, assign all z's on that line to a f(z)
+        """
+        zs = np.linspace(self.start,self.end,self.number_of_points) #this is the first value on the domain
+        return [point.point(f_z) for f_z in ist(map(self.function.evaluateAt, zs))]
+
+
 
     def points_at_step(self,n):
         list_of_tuples=[pt.get_location_at_step(n) for pt in self.points] #get the location of every point at the n-th step
-        return np.asarray(list_of_tuples).T.tolist()
+        return np.asarray(list_of_tuples).T.tolist() #take the list's transpose
     
     def parameterize_points(self,function,steps=None):
         """New function for the points on the line to draw to"""
         if not steps:
             steps=self.number_of_steps
         points=[]#temp list to store results
+        self.function=function
         for point in self.points:#take every point that has been defined by the create_points method
-            f_z = function.evaluateAt(point.complex) #evaluate the function at this complex number
+            f_z = self.function.evaluateAt(point.complex) #evaluate the function at this complex number
             point.parameterize(f_z,steps) #call on the point to parameterize itself given a new endpoint
             points.append(point) #throw that into a temp list
         self.points=points #reassign the old set of points to the temporary one. The temp list was used as the old points were being iterated over concurrently
